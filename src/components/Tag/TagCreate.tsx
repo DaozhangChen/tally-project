830123093ed1024dbc86f67caa75b102b65fe355
DaoZhang1588 <1205308740@qq.com>
2022-10-14 21:01:00 +0800

    完成itemList页面的双向绑定，出现bug页眉会横向移动

--- src/components/Tag/TagCreate.tsx
+++ src/components/Tag/TagCreate.tsx
@@ -1,15 +1,24 @@
-import {defineComponent} from "vue";
+import {defineComponent, ref, watch} from "vue";
 import {MainLayout} from "../../shared/MainLayout";
 import comeback from "../../assets/icons/comeback.svg";
 import {RouterView, useRouter} from "vue-router";
 import s from './TagCreate.module.scss'
+import {IconList} from "../../shared/IconList";
 
 export const TagCreatePage=defineComponent({
-    setup: () => {
+    setup: (props,context) => {
         const router=useRouter()
         const back=()=>{
             router.back()
         }
+        const Icon=ref<string>()
+        const IconChange=(value:string)=>{
+            Icon.value=value
+        }
+        watch(Icon,()=>{
+            console.log(Icon.value)
+        })
+        console.log(Icon.value)
         return () => (
             <MainLayout>{{
                 title: () => '新建标签',
@@ -17,13 +26,13 @@ export const TagCreatePage=defineComponent({
                 default: () =><div class={s.wrapper}>
                     <div class={s.mainCreateTag}>
                     <header class={s.inputClass}>
-                        <span class={s.tagNameClass}>标签名</span>
+                        <span class={s.tagNameClass}>标签名 {Icon.value}</span>
                         <input type='text' placeholder='2到4个汉字'/>
                         <span class={s.blank}>　</span>
                     </header>
-                    <main>
-                        <span>符号</span>
-                        <div>各种emoji</div>
+                    <main class={s.mainClass}>
+                        <IconList v-model:modelValue={Icon.value} />
+                        <div class={s.secondBlank}>　</div>
                     </main>
                     <footer class={s.upButton}>
                         <button class={s.buttonClass}>确定</button>
---
+++ src/shared/IconList.tsx
@@ -0,0 +1,74 @@
+import {computed, defineComponent, PropType, ref, watch} from "vue";
+import s from './IconList.module.scss'
+import {emojiList} from "./emojiList";
+
+export const IconList =defineComponent({
+    props:{
+        modelValue:{
+            type:String as PropType<string>
+        }
+    },
+    emits:['update:modelValue'],
+    setup:(props,context)=>{
+        //标签栏
+        const table: [string, string[]][] = [
+            ['表情', ['face-smiling', 'face-affection', 'face-tongue', 'face-hand',
+                'face-neutral-skeptical', 'face-sleepy', 'face-unwell', 'face-hat',
+                'face-glasses', 'face-concerned', 'face-negative', 'face-costume'
+            ]],
+            ['手势', ['hand-fingers-open', 'hand-fingers-partial', 'hand-single-finger',
+                'hand-fingers-closed', 'hands', 'hand-prop', 'body-parts']],
+            ['人物', ['person', 'person-gesture', 'person-role', 'person-fantasy',
+                'person-activity', 'person-sport', 'person-resting']],
+            ['衣服', ['clothing']],
+            ['动物', ['cat-face', 'monkey-face', 'animal-mammal', 'animal-bird',
+                'animal-amphibian', 'animal-reptile', 'animal-marine', 'animal-bug']],
+            ['植物', ['plant-flower', 'plant-other']],
+            ['自然', ['sky & weather', 'science']],
+            ['食物', [
+                'food-fruit', 'food-vegetable', 'food-prepared', 'food-asian',
+                'food-marine', 'food-sweet'
+            ]],
+            ['运动', ['sport', 'game']],
+        ]
+        const navIndex=ref(0)
+
+        //内部icon
+        const refSelectIcon=(emoji:string)=>{
+            context.emit('update:modelValue',emoji)
+        }
+
+
+        const emojis=computed(()=>{
+            const emojiListTitle=table[navIndex.value][1]
+            return emojiListTitle
+                .map(nav=>emojiList
+                    .find(iconList => iconList[0]===nav)?.[1]
+                    .map( icon => <li class={props.modelValue===icon ? s.selected:''}
+                        onClick={()=>{refSelectIcon(icon)}}>
+                        {icon}
+                    </li>))
+
+        })
+
+
+        return ()=>(
+            <div class={s.wrapper}>
+                <span class={s.signClass}>符号</span>
+                <main class={s.mainListClass}>
+                    <ul class={s.listTag}>
+                        {table.map((item,index)=>
+                        <li class={navIndex.value===index ? s.selected : ''}
+                            onClick={()=>navIndex.value=index}>
+                            {item[0]}
+                        </li>
+                        )}
+                    </ul>
+                    <ul class={s.mainEmojiListClass}>
+                        {emojis.value}
+                    </ul>
+                </main>
+            </div>
+        )
+    }
+})
