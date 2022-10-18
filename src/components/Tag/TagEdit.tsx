83adf33f4762e694ae4b8a372c61e71f1a910819
DaoZhang1588 <1205308740@qq.com>
2022-10-18 19:38:54 +0800

    完成标签的删除功能

--- src/components/Tag/TagEdit.tsx
+++ src/components/Tag/TagEdit.tsx
@@ -1,15 +1,34 @@
 import {defineComponent} from "vue";
 import comeback from "../../assets/icons/comeback.svg";
 import {MainLayout} from "../../shared/MainLayout";
-import {useRouter} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import {IconForm} from "../../shared/IconForm";
 import s from './TagEdit.module.scss'
+import {http} from "../../shared/Http";
+import 'vant/es/dialog/style'
+import 'vant/es/toast/style'
+import {Dialog, Toast} from "vant";
 
 export const TagEdit=defineComponent({
     setup:()=>{
         const router=useRouter()
+        const route=useRoute()
         const back=()=>{
             router.back()
+        }
+        console.log(route.query.id)
+        const deleteTag=()=>{
+            Dialog.confirm({title:'提醒',message:`确定要删除吗？\n该标签对应的记账也会随之删除`})
+                .then(()=>{
+                    http.delete(`/tags/${route.query.id}`)
+                        .then(()=>{
+                        Toast.success('删除成功')
+                            setTimeout(()=>{
+                                router.back()
+                            },1000)
+                    })
+                })
+                .catch(()=>{return})
         }
         return ()=>(
             <MainLayout>{{
@@ -18,7 +37,7 @@ export const TagEdit=defineComponent({
                 default: () =><>
                 <IconForm buttonType='edit'/>
                 <div class={s.upDeleteButton}>
-                <button class={s.deleteButton}>删除记账和标签</button>
+                <button class={s.deleteButton} onClick={deleteTag}>删除记账和标签</button>
                 </div>
                 </>
             }}</MainLayout>
